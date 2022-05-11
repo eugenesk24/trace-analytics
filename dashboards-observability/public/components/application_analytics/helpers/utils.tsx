9f55b9d584a29010f6bef842f1007a3141855f13
Eugene Lee <eugenesk@amazon.com>
2022-05-11 13:31:20 -0700

    Handle encountering deleted visualizations
    Signed-off-by: Eugene Lee <eugenesk@amazon.com>

--- dashboards-observability/public/components/application_analytics/helpers/utils.tsx
+++ dashboards-observability/public/components/application_analytics/helpers/utils.tsx
@@ -208,116 +208,123 @@ export const calculateAvailability = async (
   const visWithAvailability = [];
   let availabilityFound = false;
   for (let i = 0; i < savedVisualizationsIds.length; i++) {
+    let skip = false;
     const visualizationId = savedVisualizationsIds[i];
+    const handleDeletedViz = (value: string) => {
+      skip = true;
+      if (availabilityVisId === value.substring(value.indexOf(':') + 1)) {
+        console.log('should remove availability vis id');
+      }
+    };
     // Fetches data for visualization
-    const visData = await fetchVisualizationById(http, visualizationId, (value: string) =>
-      console.error(value)
-    );
+    const visData = await fetchVisualizationById(http, visualizationId, handleDeletedViz);
     // If there are levels, we get the current value
-    if (visData.user_configs.availabilityConfig?.hasOwnProperty('level')) {
-      // For every saved visualization with availability levels we push it to visWithAvailability
-      // This is used to populate the options in configuration
-      visWithAvailability.push({ value: visualizationId, text: visData.name });
+    if (!skip) {
+      if (visData.user_configs.availabilityConfig?.hasOwnProperty('level')) {
+        // For every saved visualization with availability levels we push it to visWithAvailability
+        // This is used to populate the options in configuration
+        visWithAvailability.push({ value: visualizationId, text: visData.name });
 
-      const levels = visData.user_configs.availabilityConfig.level.reverse();
-      let currValue = Number.MIN_VALUE;
-      const finalQuery = preprocessQuery({
-        rawQuery: visData.query,
-        startTime: visData.selected_date_range.start,
-        endTime: visData.selected_date_range.end,
-        timeField: visData.timeField,
-        isLiveQuery: false,
-      });
-      await pplService
-        .fetch({
-          query: finalQuery,
-          format: 'viz',
-        })
-        .then((res) => {
-          const stat = res.metadata.fields.filter(
-            (field: { name: string; type: string }) => !field.name.match(SPAN_REGEX)
-          )[0].name;
-          const value = res.data[stat];
-          currValue = value[value.length - 1];
-        })
-        .catch((err) => {
-          console.error(err);
+        const levels = visData.user_configs.availabilityConfig.level.reverse();
+        let currValue = Number.MIN_VALUE;
+        const finalQuery = preprocessQuery({
+          rawQuery: visData.query,
+          startTime: visData.selected_date_range.start,
+          endTime: visData.selected_date_range.end,
+          timeField: visData.timeField,
+          isLiveQuery: false,
         });
-      for (let j = 0; j < levels.length; j++) {
-        const level = levels[j];
-        // If there is an availiabilityVisId selected we only want to compute availability based on that
-        if (availabilityVisId ? availabilityVisId === visualizationId : true) {
-          if (level.value !== null) {
-            if (currValue === null) {
-              availability = {
-                name: '',
-                color: 'null',
-                mainVisId: '',
-              };
-            } else {
-              if (!availabilityFound) {
-                const expression = level.expression;
-                switch (expression) {
-                  case '≥':
-                    if (currValue >= parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
-                  case '≤':
-                    if (currValue <= parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
-                  case '>':
-                    if (currValue > parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
-                  case '<':
-                    if (currValue < parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
-                  case '=':
-                    if (currValue === parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
-                  case '≠':
-                    if (currValue !== parseFloat(level.value)) {
-                      availability = {
-                        name: level.name,
-                        color: level.color,
-                        mainVisId: visualizationId,
-                      };
-                      availabilityFound = true;
-                    }
-                    break;
+        await pplService
+          .fetch({
+            query: finalQuery,
+            format: 'viz',
+          })
+          .then((res) => {
+            const stat = res.metadata.fields.filter(
+              (field: { name: string; type: string }) => !field.name.match(SPAN_REGEX)
+            )[0].name;
+            const value = res.data[stat];
+            currValue = value[value.length - 1];
+          })
+          .catch((err) => {
+            console.error(err);
+          });
+        for (let j = 0; j < levels.length; j++) {
+          const level = levels[j];
+          // If there is an availiabilityVisId selected we only want to compute availability based on that
+          if (availabilityVisId ? availabilityVisId === visualizationId : true) {
+            if (level.value !== null) {
+              if (currValue === null) {
+                availability = {
+                  name: '',
+                  color: 'null',
+                  mainVisId: '',
+                };
+              } else {
+                if (!availabilityFound) {
+                  const expression = level.expression;
+                  switch (expression) {
+                    case '≥':
+                      if (currValue >= parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                    case '≤':
+                      if (currValue <= parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                    case '>':
+                      if (currValue > parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                    case '<':
+                      if (currValue < parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                    case '=':
+                      if (currValue === parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                    case '≠':
+                      if (currValue !== parseFloat(level.value)) {
+                        availability = {
+                          name: level.name,
+                          color: level.color,
+                          mainVisId: visualizationId,
+                        };
+                        availabilityFound = true;
+                      }
+                      break;
+                  }
                 }
               }
             }
